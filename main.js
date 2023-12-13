b0f05d6f92b03edde9ecd882a261fd4dd0d01c65
Kayode02 <144254754+Kayode02@users.noreply.github.com>
2023-12-13 11:00:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    
+    $(`#telefone`).mask(`(00) 00000-0000`) 
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+
+
+    $(`form`).validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            tell: {
+                required: true
+            },
+            email: {
+                required: true, 
+                email: true
+            },
+            messages: {
+                nome: `Por favor, insira seu nome`
+            },
+        }
+    })
+})
